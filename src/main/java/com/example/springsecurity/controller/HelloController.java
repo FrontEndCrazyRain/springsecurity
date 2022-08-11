a7b4d04fafe04b336e660a6d48354b84c4fa70dc
01cui <1147932729@qq.com>
2022-08-11 21:46:03 +0800

    项目初始化

--- src/main/java/com/example/springsecurity/controller/HelloController.java
+++ src/main/java/com/example/springsecurity/controller/HelloController.java
@@ -1,5 +1,18 @@
-package com.example.springsecurity.controller;/**
+package com.example.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author 崔令雨
  * @since 2022/8/11
- */public class HelloController {
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String login() {
+        return "hello";
+    }
+
 }
